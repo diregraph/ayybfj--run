38fd8b596f784a2bf7d54ce652811baf7996395e
Isuru Dharmadasa <isurud.14@cse.mrt.ac.lk>
2022-05-17 12:33:38 +0530

    Create solutions to the drag drop between tabs

--- src/app/tab-view/tab-view.component.ts
+++ src/app/tab-view/tab-view.component.ts
@@ -7,6 +7,8 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./tab-view.component.scss']
 })
 export class TabViewComponent implements OnInit {
+  toMove: string[] = [];
+
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
@@ -28,5 +30,4 @@ export class TabViewComponent implements OnInit {
       );
     }
   }
-
 }
